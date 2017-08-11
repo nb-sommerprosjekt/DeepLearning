bea26b9e550dbee109286100aaa73e383d9cd76b
nbnb-sommerprosjekt <knut.aron.fludal@nb.no>
2017-08-11 15:53:17 +0200

    Still some error, but work in progress

--- cnn_tf.py
+++ cnn_tf.py
@@ -2,8 +2,21 @@ import tensorflow as tf
 import numpy as np
 import pickle
 import time
-from keras.preprocessing.text import Tokenizer, one_hot
+from keras.preprocessing.text import Tokenizer, one_hot,text_to_word_sequence
+from sklearn.model_selection import train_test_split
 tf.logging.set_verbosity(tf.logging.INFO)
+
+
+def reshape_list_to_matrix(sequence):
+    sequence = np.array(sequence)
+    sequence.resize(10000)
+    sequence=sequence.reshape((500, 20))
+
+    sequence.tolist()
+    print(type(sequence))
+    return sequence
+
+
 
 #Load in the corpus
 tid=time.time()
@@ -11,13 +24,12 @@ corpus_pickle = open("clean_unique_text_stemmed_unique_new.pckl", "rb")
 corpus = pickle.load(corpus_pickle)
 (tekster, deweynr,titler) = corpus
 
-tokenizer = Tokenizer(num_words=4096,
-                      filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
-                      lower=False,
-                      split=" ",
-                      char_level=False)
-keras_corp = tokenizer.fit_on_texts(texts = tekster)
-text_matrix = tokenizer.texts_to_matrix(tekster)
+test=corpus[0][1]
+
+seq=text_to_word_sequence(test)
+
+
+
 
 tokenizer2 = Tokenizer(num_words=257,
                       filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
@@ -28,28 +40,36 @@ keras_deweynr = tokenizer2.fit_on_texts(texts = deweynr)
 dewey_matrix = tokenizer2.texts_to_matrix(deweynr)
 
 
+# prøv med den andre tokenizeren.
+tokenizer = Tokenizer(num_words=3000,
+                      filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
+                      lower=False,
+                      split=" ",
+                      char_level=False)
+keras_corp = tokenizer.fit_on_texts(texts = tekster)
+#text_matrix = tokenizer.texts_to_matrix(tekster)
 
-print(text_matrix.shape)
-print(type(text_matrix))
+
+
 #from keras import utils.data_utils
 
-X = text_matrix
+X = tekster
 Y = dewey_matrix
 mask = np.random.rand(len(X)) < 0.8
 
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y,test_size=0.2, random_state=1)
 
-X_train=X[mask]
-X_test=X[~mask]
-Y_train=Y[mask]
-Y_test=Y[~mask]
-print(X_train.shape)
-print(X_test.shape)
-print(Y_train.shape)
-print(Y_test.shape)
+# for i, stempequence in enumerate(tokenizer.texts_to_sequences_generator(X_train)):
+#     temp = reshape_list_to_matrix(tokenizer, tokenizer.sequences_to_matrix(sequence))
+#     train_step.run(feed_dict={x: [temp], y_: [Y_train[i]], keep_prob: 0.5})
 
+temp=tokenizer.texts_to_sequences(test)
+temp=reshape_list_to_matrix(temp)
+temp=tokenizer.sequences_to_matrix(temp)
+print(len(temp))
 
 print(time.time()-tid)
-
+exit(0)
 
 #Build the actual network
 
@@ -67,19 +87,20 @@ def deepnn(x):
   # Reshape to use within a convolutional neural net.
   # Last dimension is for "features" - there is only one here, since images are
   # grayscale -- it would be 3 for an RGB image, 4 for RGBA, etc.
-  x_image = tf.reshape(x, [-1, 64, 64, 1])
+  x_image = x
 
   # First convolutional layer - maps one grayscale image to 32 feature maps.
-  W_conv1 = weight_variable([5, 5, 1, 32])
-  b_conv1 = bias_variable([32])
+  W_conv1 = weight_variable([5, 5, 3000, 500])
+  b_conv1 = bias_variable([500])
+  print(x.shape)
   h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 
   # Pooling layer - downsamples by 2X.
   h_pool1 = max_pool_2x2(h_conv1)
 
   # Second convolutional layer -- maps 32 feature maps to 64.
-  W_conv2 = weight_variable([5, 5, 32, 64])
-  b_conv2 = bias_variable([64])
+  W_conv2 = weight_variable([5, 5, 500,1000])
+  b_conv2 = bias_variable([1000])
   h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 
   # Second pooling layer.
@@ -87,10 +108,10 @@ def deepnn(x):
 
   # Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
   # is down to 7x7x64 feature maps -- maps this to 1024 features.
-  W_fc1 = weight_variable([4 * 4 * 64, 1024])
-  b_fc1 = bias_variable([1024])
+  W_fc1 = weight_variable([1000, 1000])
+  b_fc1 = bias_variable([1000])
 
-  h_pool2_flat = tf.reshape(h_pool2, [-1, 4*4*64])
+  h_pool2_flat = tf.reshape(h_pool2, [-1, 1000])
   h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
   # Dropout - controls the complexity of the model, prevents co-adaptation of
@@ -99,7 +120,7 @@ def deepnn(x):
   h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
   # Map the 1024 features to 10 classes, one for each digit
-  W_fc2 = weight_variable([1024, 257])
+  W_fc2 = weight_variable([1000, 257])
   b_fc2 = bias_variable([257])
 
   y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
@@ -108,7 +129,7 @@ def deepnn(x):
 
 def conv2d(x, W):
   """conv2d returns a 2d convolution layer with full stride."""
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+  return tf.nn.conv2d(x, W, strides=[1, 25, 1, 150], padding='SAME')
 
 
 def max_pool_2x2(x):
@@ -131,13 +152,12 @@ def bias_variable(shape):
 
 
   # Import data
-nr_words = X_train.shape[1]
+nr_words = 3000
 labels = Y_train.shape[1]
-epochs=5
-num_examples=X_train.shape[1]
+epochs=1
 display_step=0
 
-x = tf.placeholder(tf.float32, [None, nr_words])
+x = tf.placeholder(tf.float32, [None, 500, 20, 3000])
 
 
 # Define loss and optimizer
@@ -163,15 +183,18 @@ with tf.Session() as sess:
     for epoch in range(epochs):
         avg_cost = 0.
         print("epoch nr {}".format(epoch))
-        for i in range(num_examples):
-            batch_xs, batch_ys = [X_train[i]], [Y_train[i]]
-            train_step.run(feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.0})
+        for i,sequence in enumerate(tokenizer.texts_to_sequences_generator(X_train)):
+            temp = reshape_list_to_matrix(tokenizer, tokenizer.sequences_to_matrix(sequence))
+            train_step.run(feed_dict={x: [temp], y_: [Y_train[i]], keep_prob: 0.5})
         #     _, c = sess.run([train_step, cost], feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
         #     avg_cost += c/num_examples
         # if (epoch+1) % display_step == 0:
         #     print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
-        print(epoch,' accuracy %g' % accuracy.eval(feed_dict={
-            x: X_test, y_: Y_test, keep_prob: 1.0}))
+        acc=0
+        for i,sequence in enumerate(tokenizer.texts_to_sequences_generator(X_test)):
+            acc+=accuracy.eval(feed_dict={x: [reshape_list_to_matrix(tokenizer,sequence)], y_: [Y_train[i]], keep_prob: 1.0})
+
+        print(epoch,' accuracy %g' % acc/len(X_test))
 
 
 
